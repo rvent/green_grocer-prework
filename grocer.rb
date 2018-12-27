7b0f0eeaaaa86a23b532bc09479675332a283a7e
Rene Ventura <venturar729@gmail.com>
2018-12-27 17:40:12 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,11 +47,11 @@ def apply_coupons(cart, coupons)
       if new_cart[new_name] == nil
         new_cart[new_name] = new_cart[name].clone
         new_cart[new_name][:price] = coupons[i][:cost]
+        new_cart[name][:count] -= coupons[i][:num]
+        new_cart[new_name][:count] = 1
+      else
+        new_cart[new_name][:count] += 1
       end
-      #   new_cart[new_name][:count] = 1
-      # else
-      #   new_cart[new_name][:count] += 1
-      # end
       p new_name, new_cart[new_name], name#, consolidated[name]
     end
     # if new_cart[new_name][:count] == nil

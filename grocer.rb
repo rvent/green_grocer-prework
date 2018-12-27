96dd777ce9bd013ecc1e339f96d41f642205b165
Rene Ventura <venturar729@gmail.com>
2018-12-27 17:52:53 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -8,7 +8,7 @@ data =   [
 coupons =
   [
     {:item => "AVOCADO", :num => 2, :cost => 5.00},
-    {:item => "AVOCADO", :num => 2, :cost => 5.00}
+    #{:item => "AVOCADO", :num => 2, :cost => 5.00}
     # {:item => "BEER", :num => 2, :cost => 20.00},
     # {:item => "CHEESE", :num => 3, :cost => 15.00}
   ]

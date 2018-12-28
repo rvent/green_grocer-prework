0c0dcc425516417b1f8bd6cbea0c6a5b201e3dbc
Rene Ventura <venturar729@gmail.com>
2018-12-28 00:27:28 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,18 +1,3 @@
-data =   [
-    {"AVOCADO" => {:price => 3.00, :clearance => true}},
-    # {"KALE" => {:price => 3.00, :clearance => false}},
-    # {"AVOCADO" => {:price => 3.00, :clearance => true}},
-    {"AVOCADO" => {:price => 3.00, :clearance => true}}
-  ]
-
-coupons =
-  [
-    {:item => "AVOCADO", :num => 2, :cost => 5.00}
-    #{:item => "AVOCADO", :num => 2, :cost => 5.00}
-    # {:item => "BEER", :num => 2, :cost => 20.00},
-    # {:item => "CHEESE", :num => 3, :cost => 15.00}
-  ]
-
 def consolidate_cart(cart)
   # code here
   consolidated_cart = {}
@@ -21,7 +6,6 @@ def consolidate_cart(cart)
       if consolidated_cart[name] == nil
         consolidated_cart[name] = values.clone
       end
-      # p consolidated_cart[name]
       if consolidated_cart[name][:count] == nil
         consolidated_cart[name][:count] = 1
       else
@@ -32,18 +16,14 @@ def consolidate_cart(cart)
   consolidated_cart
  end
 
-# p consolidate_cart(data)
-
 def apply_coupons(cart, coupons)
   # code here
   new_cart = cart.clone
-  #consolidated = consolidate_cart(cart).clone
   item_values = coupons.map {|item| item.values}
   item_values.each_with_index do |selection, i|
     name = selection[0]
     if new_cart[name] != nil
       new_name = "#{name} W/COUPON"
-      #p"B4 w", new_cart[new_name], "B4 w/o", new_cart[name]
       if new_cart[new_name] == nil
         new_cart[new_name] = new_cart[name].clone
         new_cart[new_name][:price] = coupons[i][:cost]
@@ -55,18 +35,9 @@ def apply_coupons(cart, coupons)
         new_cart[name][:count] -= coupons[i][:num]
       end
     end
-    # if new_cart[new_name][:count] == nil
-    #   new_cart[new_name][:count] = 1
-    # else
-    #   new_cart[new_name][:count] += 1
-    # end
   end
   new_cart
 end
-
-# cart = consolidate_cart(data)
-# p cart
-# p apply_coupons(cart, coupons)
 
 def apply_clearance(cart)
   # code here
@@ -100,5 +71,3 @@ def checkout(cart, coupons)
   end
   total
 end
-
-p checkout(data, coupons)
